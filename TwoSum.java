9db5b1141c6d277361c09b885654f43886e0420d
Mysticman22 <mysticeldrago@gmail.com>
2025-09-12 01:10:49 +0530

    .

---
+++ TwoSum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class TwoSum {
+    // LeetCode signature
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(); // value -> index
+        for (int i = 0; i < nums.length; i++) {
+            int need = target - nums[i];
+            if (map.containsKey(need)) {
+                return new int[] { map.get(need), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+} 
