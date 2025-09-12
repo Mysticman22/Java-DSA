ccc2cf62a7fabeaa25246d3a8284688d669367f7
Mysticman22 <mysticeldrago@gmail.com>
2025-09-12 15:16:27 +0530

    Lheap

---
+++ KthLargest_Heap.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class KthLargest_Heap {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int x : nums) {
+            minHeap.offer(x);
+            if (minHeap.size() > k) minHeap.poll();
+        }
+        return minHeap.peek();
+    }
+
+    public static void main(String[] args) {
+        KthLargest_Heap solver = new KthLargest_Heap();
+        System.out.println(solver.findKthLargest(new int[]{3,2,1,5,6,4}, 2)); // 5
+        System.out.println(solver.findKthLargest(new int[]{3,2,3,1,2,4,5,5,6}, 4)); // 4
+    }
+}
