759cc3b0f286e2fd9da6b3880120bc5861e5a780
Mysticman22 <mysticeldrago@gmail.com>
2025-10-03 16:32:49 +0530

    .

--- ProductExceptSelf.java
+++ ProductExceptSelf.java
@@ -2,16 +2,19 @@
 import java.util.*;
 
 public class ProductExceptSelf {
+    
     // LeetCode signature
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
         int[] ans = new int[n];
+
         // prefix products
         int prefix = 1;
         for (int i = 0; i < n; i++) {
             ans[i] = prefix;
             prefix *= nums[i];
         }
+
         // multiply by suffix products
         int suffix = 1;
         for (int i = n - 1; i >= 0; i--) {
