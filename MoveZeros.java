8820f9345aeacdaa4aa1305653819c792b8df431
Mysticman22 <mysticeldrago@gmail.com>
2025-10-27 20:04:23 +0530

    Moving the zeros included in the string to the end

---
+++ MoveZeros.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class MoveZeros {
+    public static void main(String[] args) {
+        int[] a = {0, 1, 0, 3, 12}; int j = 0;
+        for (int i : a) if (i != 0) a[j++] = i;
+        while (j < a.length) a[j++] = 0;
+        System.out.println(Arrays.toString(a));
+    }
+}
