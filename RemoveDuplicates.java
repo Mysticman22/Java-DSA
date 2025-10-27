a55c986adf9a4c8f4d855c3b2882add7c5061f3e
Mysticman22 <mysticeldrago@gmail.com>
2025-10-27 20:13:30 +0530

    Removing repeated elements from string

--- RemoveDuplicates.java
+++
@@ -1,7 +0,0 @@
-public class RemoveDuplicates {
-    public static void main(String[] args) {
-        String s = "programming", res = "";
-        for (char c : s.toCharArray()) if (res.indexOf(c) == -1) res += c;
-        System.out.println(res);
-    }
-}
