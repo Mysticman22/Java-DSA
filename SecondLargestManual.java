15078c68e95e3b53f3ff5cae9ba5d368549650c3
Mysticman22 <mysticeldrago@gmail.com>
2025-11-02 22:35:14 +0530

    Finding the second largest element in array without builtin function

---
+++ SecondLargestManual.java
@@ -0,0 +1,15 @@
+public class SecondLargestManual {
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 8, 20, 15};
+        int first = Integer.MIN_VALUE, second = Integer.MIN_VALUE;
+        for (int num : arr) {
+            if (num > first) {
+                second = first;
+                first = num;
+            } else if (num > second && num != first) {
+                second = num;
+            }
+        }
+        System.out.println("Second Largest = " + second);
+    }
+}
