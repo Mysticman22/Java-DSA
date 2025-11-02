65c0b040bcc3f43622728b031b64fef022364bef
Mysticman22 <mysticeldrago@gmail.com>
2025-11-02 22:41:12 +0530

    Moving the zeroes in the array to the last

---
+++ MoveZerosManual.java
@@ -0,0 +1,15 @@
+public class MoveZerosManual {
+    public static void main(String[] args) {
+        int[] arr = {0, 3, 1, 0 ,12};
+        int j = 0;
+        for ( int i = 0; i < arr.length; i++) {
+            if (arr[i] != 0) {
+              int temp = arr[i];
+              arr[i] =  arr[j];
+              arr[j] = temp;
+              j++;
+            }
+        }
+        for(int x : arr) System.out.print(x + " ");
+    }
+}
