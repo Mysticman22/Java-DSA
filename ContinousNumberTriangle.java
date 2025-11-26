21bb603b9e9adf9b94f12a624a5cb3a5311437e2
Mysticman22 <mysticeldrago@gmail.com>
2025-11-27 00:43:38 +0530

    To print a triangle in such a way that the numbers print in sequential order

---
+++ ContinousNumberTriangle.java
@@ -0,0 +1,10 @@
+public class ContinousNumberTriangle {
+    public static void main(String[] args) {
+        int n = 4, num = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++)
+                System.out.print(num++);
+            System.out.println();
+        }
+    }
+}
