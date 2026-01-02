d9fa1e2292b1d1b388cb9b1b588a9f1abdc24613
Mysticman22 <mysticeldrago@gmail.com>
2026-01-02 23:52:42 +0530

    error changes

--- Hollowdiamond.java
+++ Hollowdiamond.java
@@ -1,5 +1,5 @@
 public class Hollowdiamond {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         int n = 4;
 
         for (int i = 1; i <= n; i++) {
