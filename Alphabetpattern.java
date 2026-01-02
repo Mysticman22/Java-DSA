fb1573bd1a032b5b05a5b4d2997e33b3a88f01a0
Mysticman22 <mysticeldrago@gmail.com>
2026-01-02 23:47:54 +0530

    pattern changes

--- Alphabetpattern.java
+++ Alphabetpattern.java
@@ -1,6 +1,6 @@
 public class Alphabetpattern{
     public static void main(String[] args) {
-        int n = 5;
+        int n = 4;
         for (int i = 0; i < n; i++) {
             char c = 'A';
             for (int j = 0; j <= i; j++)
