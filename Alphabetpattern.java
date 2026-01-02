e8eb7150e68b2ca7c441a949e1ede8b1aa65ff17
Mysticman22 <mysticeldrago@gmail.com>
2026-01-02 23:45:22 +0530

    patttern methods

--- Alphabetpattern.java
+++ Alphabetpattern.java
@@ -1,6 +1,6 @@
 public class Alphabetpattern{
     public static void main(String[] args) {
-        int n = 4;
+        int n = 5;
         for (int i = 0; i < n; i++) {
             char c = 'A';
             for (int j = 0; j <= i; j++)
