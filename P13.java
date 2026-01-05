ae6a5d8973aa8fc3081c8482a0b89291d1023b9d
Mysticman22 <mysticeldrago@gmail.com>
2026-01-05 18:40:32 +0530

    changed character

--- P13.java
+++ P13.java
@@ -2,7 +2,7 @@ public class P13 {
     public static void main(String[] args) {
         int n = 4;
         for (int i = 0; i < n; i++) {
-            char c = 'A';
+            char c = 'B';
             for (int j = 0; j <= i; j++)
                 System.out.print((char)(c+j) + " ");
             System.out.println();
