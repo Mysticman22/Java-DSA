b3bb9c9a005819382a3ffc0d544a3acecbf5b98e
Mysticman22 <mysticeldrago@gmail.com>
2026-01-11 22:44:10 +0530

    updated array

--- ReverseArray.java
+++ ReverseArray.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 public class ReverseArray {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr = {1, 2, 7, 4, 5};
         for (int i = 0, j = arr.length - 1; i < j; i++, j--) {
             int t = arr[i]; arr[i] = arr[j]; arr[j] = t;
         }
